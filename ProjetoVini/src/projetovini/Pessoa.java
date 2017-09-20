fef6d66460f89d248ce55b12cc29d6112af6b3b1
ViniciusVonAhn <viniciusvonahn@gmail.com>
2017-09-20 12:36:37 -0300

    Pessoa Abstrata

--- ProjetoVini/src/projetovini/Pessoa.java
+++ ProjetoVini/src/projetovini/Pessoa.java
@@ -2,6 +2,49 @@ package projetovini;
 /**
  * @author vinicius
  */
-public class Pessoa {
+public abstract class Pessoa {
+    
+    protected String nome;
+    protected String cpf;
+    protected int idade;
+
+    public Pessoa(String nome, String cpf, int idade) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.idade = idade;
+    }
+
+    
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" + "nome=" + nome + ", cpf=" + cpf + ", idade=" + idade + '}';
+    }
+    
+    
     
 }
