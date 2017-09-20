8ce2a117b49167068a53a81c4edb8e5ada394d62
ViniciusVonAhn <viniciusvonahn@gmail.com>
2017-09-20 12:27:07 -0300

    Funcional

--- ProjetoVini/src/Connection/ConnectionFactory.java
+++ ProjetoVini/src/Connection/ConnectionFactory.java
@@ -1,19 +1,70 @@
 package Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+
+import com.mysql.jdbc.PreparedStatement;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
- * @author vinicius
+ *
+ * @author Vinny
  */
 public class ConnectionFactory {
-    public Connection getConnection(){
+    
+    private static final String DRIVER ="com.mysql.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/mercado?autoReconnect=true&useSSL=false";
+   // private static final String URL = "jdbc:mysql://localhost:3306/mercado";
+    private static final String USER = "root";
+    private static final String PASS = "root";
+    
+    
+    public static Connection getConnection(){
         try {
+            Class.forName(DRIVER);
             
-            return DriverManager.getConnection("jdbc:mysql://localhost:1527/projeto","root", "");
-        } catch (Exception ex) {
-            throw new RuntimeException(ex);
+            return DriverManager.getConnection(URL, "root", "root");
+            
+        } catch (ClassNotFoundException | SQLException ex) {
+          throw new RuntimeException("Erro na conexão: ",ex);
         }
     }
+    public static void closeConnection(Connection con){
+        try {
+        if(con != null){
+        con.close();
+            }
+            } catch (SQLException ex) {
+                Logger.getLogger(ConnectionFactory.class.getName()).log(Level.SEVERE, null, ex);
+            }
+    }
+    
+    public static void closeConnection(Connection con, PreparedStatement stmt){
+        
+        closeConnection(con);
+        try {
+            if(stmt != null){
+                stmt.close();
+            }
+        
+            } catch (SQLException ex) {
+                Logger.getLogger(ConnectionFactory.class.getName()).log(Level.SEVERE, null, ex);
+            }
+    }
+    
+    public static void closeConnection(Connection con, PreparedStatement stmt, ResultSet rs){
+        
+        closeConnection(con, stmt);
+        try {
+            if(rs !=null){
+                rs.close();
+            }
+        
+            } catch (SQLException ex) {
+                Logger.getLogger(ConnectionFactory.class.getName()).log(Level.SEVERE, null, ex);
+            }
+    }
+    
 }
-
--- ProjetoVini/src/Connection/Testando.java
+++ ProjetoVini/src/Connection/Testando.java
@@ -1,16 +1,27 @@
 package Connection;
+
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Connection;
-/** *
+
+/**
+ * *
  * @author vinicius
  */
 public class Testando {
-    public static void main(String[] args) throws SQLException{
-        Connection co = new ConnectionFactory().getConnection();
-        System.out.println("Conecção aberta");
-        
-       
+
+    public static void main(String[] args) throws SQLException {
+        Produto p = new Produto();
+        ProdutoDAO dao = new ProdutoDAO();
+        p.setDescricao("Maionese");
+        p.setQtd(7);
+        p.setPreco(2.35);
+        dao.create(p);
+
+        for (Produto a : dao.read()) {
+            System.out.println(a.getId() + " " + a.getDescricao() + " " + a.getQtd() + " " + a.getPreco());
+
+        }
+
     }
- 
 }
